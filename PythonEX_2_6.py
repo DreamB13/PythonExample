e2f9f316a9f2f43eb2f20e5483f899b9949a603b
DreamB <luisluos@gmail.com>
2024-10-10 17:58:25 +0900

    241010

---
+++ PythonEX_2_6.py
@@ -0,0 +1,17 @@
+# 문자열을 숫자로 바꾸기
+string_a = input("입력 A: ")
+int_a = int(string_a)
+
+string_b = input("입력 B: ")
+int_b = int(string_b)
+
+print("문자열 자료: ", string_a + string_b)
+print("숫자 자료: ", int_a + int_b)
+
+# float
+
+output_a = int("52")
+output_b = float("52.273")
+
+print(type(output_a), output_a)
+print(type(output_b), output_b)
---
+++ PythonEX_2_7.py
@@ -0,0 +1,8 @@
+# cm 를 인치로 바꾸기
+# cm입력
+raw_input = int(input("cm 입력: "))
+
+inch_output = raw_input * 2.54
+
+# inch출력
+print(raw_input, "cm는", inch_output, "inch 입니다.")
