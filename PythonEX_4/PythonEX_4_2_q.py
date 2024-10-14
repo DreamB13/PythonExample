a595131b113ad5b3df16e18535137edfa4955fbf
DreamB13 <luisluos@gmail.com>
2024-10-14 23:55:11 +0900

    Update PythonEX_4_2_q.py

--- PythonEX_4/PythonEX_4_2_q.py
+++ PythonEX_4/PythonEX_4_2_q.py
@@ -1,34 +1,35 @@
 # 3번 문제 ---------------------------------------------------------------------
 
-# # numbers = [1,2,8,6,4,5,5,2,1,4,5,6,8,1,2,3,6,4,5,4,7,7,2,9,2,1]
-# # counter = {}
+numbers = [1,2,8,6,4,5,5,2,1,4,5,6,8,1,2,3,6,4,5,4,7,7,2,9,2,1]
+counter = {}
 
-# # for number in numbers:
-# #     a = number
-# #     if a == number:
-# #         number_count += 1
-# #     counter[a] = number_count
-
-
-
-# print(counter)
+for number in numbers:
+    count_num = 0
+    counter[number] = count_num
+    if number in counter[number]:
+        
+        
+print(counter)
 
 # 4번 문제 ---------------------------------------------------------------------
-character = {
-    "name": "기사",
-    "level": 12,
-    "items": {
-        "sword": "불꽃의 검",
-        "armor": "풀플레이트"
-    },
-    "skill": ["베기", "세게 베기", "아주 세게 베기"]
-}
+# character = {
+#     "name": "기사",
+#     "level": 12,
+#     "items": {
+#         "sword": "불꽃의 검",
+#         "armor": "풀플레이트"
+#     },
+#     "skill": ["베기", "세게 베기", "아주 세게 베기"]
+# }
 
-for key in character:
-    if type(character[key]) is str:
-        print(key,":",[key])
-    elif type(character[key]) is dict:
-        print(character[key])
-    elif type(character[key]) is list:
-        skill = character[key]
-        print(character[key],":",skill[])
+# for key in character:
+#     if type(character[key]) is str:
+#         print(key,":",character[key])
+#     elif type(character[key]) is int:
+#         print(key,":",character[key])
+#     elif type(character[key]) is dict:
+#         for i in character[key]:
+#             print(i,":",character[key][i])
+#     elif type(character[key]) is list:
+#         for j in character[key]:
+#             print(key,":",j)
