d025bdaaaf6f54528b191a7c820a722424b14281
DreamB <luisluos@gmail.com>
2024-10-15 18:52:17 +0900

    20241015

--- PythonEX_4/PythonEX_4_10.py
+++ PythonEX_4/PythonEX_4_10.py
@@ -1,6 +1,9 @@
-# 피라미드 만들기
-height = int(input("높이 지정: "))
-line_of_ast = 2*height-1
+# 피라미드 만들기 no.1
+height = int(input("몇 줄까지 만들까요? >"))
 
 for i in range(height):
-    
+    for j in range(height-i):
+        print(" ",end="")
+    for k in range((i*2)+1):
+        print("*",end="")
+    print("")
