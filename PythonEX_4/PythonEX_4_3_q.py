87787315712ee79e7f17a646d8560b223d168005
DreamB <luisluos@gmail.com>
2024-10-21 17:25:30 +0900

    20241021

---
+++ PythonEX_4/PythonEX_4_3_q.py
@@ -0,0 +1,14 @@
+# 2번 문제 ------------------------------------
+key_list = ["name", "hp", "mp", "level"]
+value_list = ["기사", 200, 30, 5]
+character= {}
+
+for i in range(4):
+    character[key_list[i]]=value_list[i]
+
+print(character)
+
+#3번 문제 -----------------------------------
+a = 3+2
+a.sum()
+print(a)
